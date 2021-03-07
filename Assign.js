3e8017f3463b8f53c98fc0be9331cc013d606fa9
mercy021 <kinyanjuimercywangari@gmail.com>
2021-03-07 18:19:56 +0300

    attempt one

--- Assign.js
+++ Assign.js
@@ -1,7 +1,7 @@
   function fruits(fruits_list){
     fruits_list=["strawberries","kiwi","guava","mangoes","tomatoes"];
     fruits_list.forEach(element=>console.log(element));
-    console.log(fruits_list);
+    console.log(fruits_list[0]);
   } 
   fruits();
 
