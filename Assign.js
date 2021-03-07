6af48d8cbdd21432ee6d04ae1e003bffb1f3892e
mercy021 <kinyanjuimercywangari@gmail.com>
2021-03-07 18:09:04 +0300

    first commit

---
+++ Assign.js
@@ -0,0 +1,10 @@
+  function fruits(fruits_list){
+    fruits_list=["strawberries","kiwi","guava","mangoes","tomatoes"];
+    fruits_list.forEach(element=>console.log(element));
+    console.log(fruits_list);
+  } 
+  fruits();
+
+
+
+
